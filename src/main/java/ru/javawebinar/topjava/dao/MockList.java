0714ddc0abfb61b9465c6b53d5dd4f3a1e220fd9
yur1st <okhvat@gmail.com>
2020-06-04 23:13:55 +0300

    Implement mandatory HW0 task

---
+++ src/main/java/ru/javawebinar/topjava/dao/MockList.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.dao;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+public final class MockList {
+    public static final List<Meal> mealList;
+
+    static {
+        mealList = Arrays.asList(
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+        );
+    }
+
+}
